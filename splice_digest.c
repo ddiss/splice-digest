97425fdbb3785a42e318f7acd434395b2fb12a79
David Disseldorp <ddiss@suse.de>
2024-10-11 02:34:25 +0000

    splice_digest: use 1M splice size and set F_SETPIPE_SZ
    Kernel /proc/sys/fs/pipe-max-size is 1M by default, while the default
    pipe buffer size is 64K. An explicit F_SETPIPE_SZ(1M) call is needed to
    increase the buffer size to the pipe-max-size.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- splice_digest.c
+++ splice_digest.c
@@ -1,6 +1,8 @@
-/* SPDX-License-Identifier: MIT */
-
-/* File digests using the kernel's AF_ALG API with splice(). */
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2024 SUSE LLC.
+ * Generate file digests using the kernel's AF_ALG API with splice().
+ */
 #define _GNU_SOURCE
 #include <fcntl.h>
 #include <err.h>
@@ -14,7 +16,7 @@
 #include <unistd.h>
 #include <linux/if_alg.h>
 
-#define SPLICE_MAX (1024 * 1024 * 2)
+#define SPLICE_MAX (1024 * 1024)
 
 void print_hash_result(int outfd)
 {
@@ -36,7 +38,7 @@ int main(int argc, char *argv[])
 	const char *infile;
 	int infd, sfd, outfd;
 	size_t alg_len, insize;
-	ssize_t got;
+	ssize_t got, l;
 	struct stat st;
 	int pipefds[2];
 	struct sockaddr_alg sa = {
@@ -88,9 +90,13 @@ int main(int argc, char *argv[])
 	/* can't splice directly from infd to the AF_ALG socket; pipe needed */
 	if (pipe(pipefds) < 0)
 		err(-1, "pipe");
+	if (fcntl(pipefds[0], F_SETPIPE_SZ, SPLICE_MAX) < 0
+	 || fcntl(pipefds[1], F_SETPIPE_SZ, SPLICE_MAX) < 0)
+		fprintf(stderr, "F_SETPIPE_SZ(%llu) failed, using default. "
+			"Check /proc/sys/fs/pipe-max-size\n", SPLICE_MAX);
 
 	for (insize = st.st_size; insize; insize -= got) {
-		size_t l, tryl = (insize < SPLICE_MAX ? insize : SPLICE_MAX);
+		size_t tryl = (insize < SPLICE_MAX ? insize : SPLICE_MAX);
 
 		l = splice(infd, NULL, pipefds[1], NULL, tryl, 0);
 		if (l <= 0 || l > tryl)
